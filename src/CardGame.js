ff7dac6d1ae2a21a01cfdc35cc8e4f6a2477befb
Yuri <yuri.belorusets@gmail.com>
2022-05-03 11:14:42 -0700

    up to first code review

---
+++ src/CardGame.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+/** Component for card-draw game
+ * state: deck, cards
+ */
+function CardGame() {
+    const [deck, setDeck] = useState([]);
+    const [cards, setCards] = useState([]);
+
+    useEffect(function getDeck() {
+
+        async function createDeck() {
+            const response = await axios.get("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+            setDeck(response.data);
+        }
+
+        createDeck();
+    }, []);
+
+    async function drawCard() {
+        if (deck.remaining === 0) return alert("No more cards to draw!");
+
+        const response = await axios.get("http://deckofcardsapi.com/api/deck/" + deck.deck_id + "/draw/?count=1")
+        setCards([...cards, response.data.cards[0]]);
+        setDeck({...deck, ["remaining"] : response.data.remaining});
+    }
+    
+
+    return (
+        <div>
+            <button onClick={drawCard}>Draw Card</button>
+
+            {53 > cards.length > 0 && <section className="cardSection">
+                {cards.map(card => <img key={card.code} className="card" src={card.image} ></img>)}
+            </section>}
+        </div>
+
+    )
+}
+
+export default CardGame;
